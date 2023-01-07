0b5a9a1bf1728106b1e2d0caf4ff842f2a53d20c
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2023-01-07 18:23:35 -0300

    refactor: change auth use case to recieve a model

--- lib/domain/usecases/authentication.dart
+++ lib/domain/usecases/authentication.dart
@@ -2,9 +2,16 @@ import 'package:flutter/cupertino.dart';
 
 import '../entities/entities.dart';
 
+abstract class AuthParams {
+  final String email;
+  final String secret;
+
+  AuthParams({
+    @required this.email,
+    @required this.secret
+  });
+}
+
 abstract class Authentication {
-  Future<Account> auth({
-      @required String email, 
-      @required String password
-    });
+  Future<Account> auth(AuthParams params);
 }
