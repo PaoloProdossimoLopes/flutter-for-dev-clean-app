583f0abae6c92f4f713e05dac8147cf1c76c4017
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2023-01-07 17:54:12 -0300

    test: ensure the client was called with correct values

---
+++ test/data/usecases/remote_authentication_test.dart
@@ -0,0 +1,44 @@
+import 'dart:io';
+
+import 'package:ForDev/domain/entities/account.dart';
+import 'package:ForDev/domain/usecases/authentication.dart';
+import 'package:faker/faker.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+
+class RemoteAuthentication {
+  final HTTPClient client;
+  final String url;
+
+  RemoteAuthentication({
+    @required this.client,
+    @required this.url
+  });
+  
+  Future<void> auth() async {
+    await client.request(url: url, method: "POST");
+  }
+}
+
+abstract class HTTPClient {
+  Future<void> request({String url, String method});
+}
+
+void main() {
+  test('should call http client with correct values (url and method)', () async {
+    final httpClient = HTTPClientSpy();
+    final url = faker.internet.httpUrl();
+    final method = 'POST';
+    final sut = RemoteAuthentication(client: httpClient, url: url);
+
+    await sut.auth();
+
+    verify(httpClient.request(url: url, method: method));
+  });
+}
+
+class HTTPClientSpy extends Mock implements HTTPClient {
+
+}
