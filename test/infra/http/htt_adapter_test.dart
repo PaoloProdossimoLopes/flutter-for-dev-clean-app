2c3630b0329e28aba995155154a660c7dcb0efd9
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2023-01-09 15:03:35 -0300

    test: ensure the body is passed when not passed for request

--- test/infra/http/htt_adapter_test.dart
+++ test/infra/http/htt_adapter_test.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:faker/faker.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart';
 import 'package:mockito/mockito.dart';
@@ -12,13 +15,14 @@ class HTTPAdapter {
   HTTPAdapter(this.client);
 
   Future<void> request({
-    String url,
-    String method
+    @required String url,
+    @required String method,
+    Map body
   }) async {
     client.post(url,  headers: {
         'content-type': 'application/json',
         'accept': 'application/json'
-        });
+        }, body: body != null ? jsonEncode(body) : null);
   }
 }
 
@@ -41,6 +45,24 @@ void main() {
         'content-type': 'application/json',
         'accept': 'application/json'
         }));
+    });
+
+     test('should call post with no body', () async {
+      sut.request(url: url, method: "POST");
+
+      verify(client.post(url, headers: {
+        'content-type': 'application/json',
+        'accept': 'application/json'
+        }, body: null));
+    });
+
+    test('should call post with body', () async {
+      sut.request(url: url, method: "POST", body: { "any_key": "any_value" });
+
+      verify(client.post(url, headers: {
+        'content-type': 'application/json',
+        'accept': 'application/json'
+        }, body: '{"any_key":"any_value"}'));
     });
   });
 }
