4d4f31c7ac09c386e715c51d6f1f1b9db8c27ada
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2023-01-09 15:29:17 -0300

    test: ensure the body is passed if needed

--- test/infra/http/htt_adapter_test.dart
+++ test/infra/http/htt_adapter_test.dart
@@ -1,68 +1,70 @@
-import 'dart:convert';
+  import 'dart:convert';
 
-import 'package:faker/faker.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:http/http.dart';
-import 'package:mockito/mockito.dart';
+  import 'package:faker/faker.dart';
+  import 'package:flutter/cupertino.dart';
+  import 'package:flutter_test/flutter_test.dart';
+  import 'package:http/http.dart';
+  import 'package:mockito/mockito.dart';
 
-class ClientSpy extends Mock implements Client { }
+  class ClientSpy extends Mock implements Client { }
 
-class HTTPAdapter {
+  class HTTPAdapter {
 
-  final Client client;
+    final Client client;
 
-  HTTPAdapter(this.client);
+    HTTPAdapter(this.client);
 
-  Future<void> request({
-    @required String url,
-    @required String method,
-    Map body
-  }) async {
-    client.post(url,  headers: {
-        'content-type': 'application/json',
-        'accept': 'application/json'
-        }, body: body != null ? jsonEncode(body) : null);
+    Future<void> request({
+      @required String url,
+      @required String method,
+      Map body
+    }) async {
+      const headers =  {
+          'content-type': 'application/json',
+          'accept': 'application/json'
+          };
+      final bodyIfNedded = body != null ? jsonEncode(body) : null;
+      client.post(url, headers: headers, body: bodyIfNedded);
+      }
   }
-}
 
-void main() {
-  ClientSpy client;
-  HTTPAdapter sut;
-  String url;
+  void main() {
+    ClientSpy client;
+    HTTPAdapter sut;
+    String url;
 
-  setUp(() {
-    client = ClientSpy();
-    sut = HTTPAdapter(client);
-    url = faker.internet.httpUrl();
-  });
-
-  group('POST', () {
-    test('should call post with correct values', () async {
-      sut.request(url: url, method: "POST");
-
-      verify(client.post(url, headers: {
-        'content-type': 'application/json',
-        'accept': 'application/json'
-        }));
+    setUp(() {
+      client = ClientSpy();
+      sut = HTTPAdapter(client);
+      url = faker.internet.httpUrl();
     });
 
-     test('should call post with no body', () async {
-      sut.request(url: url, method: "POST");
+    group('POST', () {
+      test('should call post with correct values', () async {
+        sut.request(url: url, method: "POST");
 
-      verify(client.post(url, headers: {
-        'content-type': 'application/json',
-        'accept': 'application/json'
-        }, body: null));
+        verify(client.post(url, headers: {
+          'content-type': 'application/json',
+          'accept': 'application/json'
+          }));
+      });
+
+      test('should call post with no body', () async {
+        sut.request(url: url, method: "POST");
+
+        verify(client.post(url, headers: {
+          'content-type': 'application/json',
+          'accept': 'application/json'
+          }, body: null));
+      });
+
+      test('should call post with body', () async {
+        sut.request(url: url, method: "POST", body: { "any_key": "any_value" });
+
+        verify(client.post(url, headers: {
+          'content-type': 'application/json',
+          'accept': 'application/json'
+          }, body: '{"any_key":"any_value"}'));
+      });
     });
-
-    test('should call post with body', () async {
-      sut.request(url: url, method: "POST", body: { "any_key": "any_value" });
-
-      verify(client.post(url, headers: {
-        'content-type': 'application/json',
-        'accept': 'application/json'
-        }, body: '{"any_key":"any_value"}'));
-    });
-  });
-}
+  }
