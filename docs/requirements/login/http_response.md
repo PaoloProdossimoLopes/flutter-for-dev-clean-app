5a429a979183b7c21f4eee1bef64b4e35ee022b4
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2023-01-09 14:46:33 -0300

    test: ensure HTTPAdapter calls post with corerect url

---
+++ docs/requirements/login/http_response.md
@@ -0,0 +1,23 @@
+# HTTP
+
+> ## Sucesso
+1. Request com verbo http valido (post)
+2. Passar nos headers o content type JSON
+3. Ok - 200 e a resposta com dados
+4. No content - 204 e resposta sem dados
+
+> ## Erros
+1. Bad request - 400
+2. Unauthorized - 401
+3. Forbidden - 403
+4. Not found - 404
+5. Internal server error - 500
+
+## Exceçao = Status coode diferente dos citados acima
+1. Internal server error - 500
+
+## Exceçao - HTTP request deu alguma excesao
+1. Internal server error - 500
+
+## Exceçao - Verbo HTTP invalido
+1. Internal server error - 500
---
+++ test/infra/http/htt_adapter_test.dart
@@ -0,0 +1,32 @@
+import 'package:faker/faker.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart';
+import 'package:mockito/mockito.dart';
+
+class ClientSpy extends Mock implements Client { }
+
+class HTTPAdapter {
+
+  final Client client;
+
+  HTTPAdapter(this.client);
+
+  Future<void> request({
+    String url,
+    String method
+  }) async {
+    client.post(url);
+  }
+}
+
+void main() {
+  group('POST', () {
+    test('should call post with correct values', () async {
+      final url = faker.internet.httpUrl();
+      final client = ClientSpy();
+      final sut = HTTPAdapter(client);
+      sut.request(url: url, method: "POST");
+      verify(client.post(url));
+    });
+  });
+}
