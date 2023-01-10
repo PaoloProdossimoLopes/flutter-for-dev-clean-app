34d7faa3fab04edca981284d3bf526526eee2f31
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2023-01-10 11:00:13 -0300

    test: ensure feat: deleivers bad request on status code 400

--- test/infra/http/htt_adapter_test.dart
+++ test/infra/http/htt_adapter_test.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:ForDev/domain/helpers/domain_error.dart';
 import 'package:ForDev/infra/http/http_adapter.dart';
 import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -17,14 +20,14 @@ void main() {
     url = faker.internet.httpUrl();
   });
 
-  mock_post_with_data(int statusCode, String response) {
+  mock_post_with(int statusCode, String response) {
     when(client.post(url, headers: anyNamed('headers'), body: anyNamed('body')))
         .thenAnswer((_) async => Response(response, statusCode));
   }
 
   group('POST', () {
     test('should call post with correct values', () async {
-      mock_post_with_data(200, '{"any_key":"any_value"}');
+      mock_post_with(200, '{"any_key":"any_value"}');
 
       sut.request(url: url, method: "POST");
 
@@ -35,7 +38,7 @@ void main() {
     });
 
     test('should call post with no body', () async {
-      mock_post_with_data(200, '{"any_key":"any_value"}');
+      mock_post_with(200, '{"any_key":"any_value"}');
 
       sut.request(url: url, method: "POST");
 
@@ -48,7 +51,7 @@ void main() {
     });
 
     test('should call post with body', () async {
-      mock_post_with_data(200, '{"any_key":"any_value"}');
+      mock_post_with(200, '{"any_key":"any_value"}');
 
       sut.request(url: url, method: "POST", body: {"any_key": "any_value"});
 
@@ -61,7 +64,7 @@ void main() {
     });
 
     test('request return data if post returns 200', () async {
-      mock_post_with_data(200, '{"any_key":"any_value"}');
+      mock_post_with(200, '{"any_key":"any_value"}');
 
       final response = await sut.request(url: url, method: 'POST');
 
@@ -69,7 +72,7 @@ void main() {
     });
 
     test('request deleivers no resposne with 204 with no datas', () async {
-      mock_post_with_data(204, '');
+      mock_post_with(204, '');
 
       final response = await sut.request(url: url, method: 'POST');
 
@@ -78,11 +81,23 @@ void main() {
 
     test('request deleivers no resposnes with status code with 204 with data',
         () async {
-      mock_post_with_data(204, '{"any_key":"any_value"}');
+      mock_post_with(204, '{"any_key":"any_value"}');
 
       final response = await sut.request(url: url, method: 'POST');
 
       expect(response, null);
+    });
+
+    test('request returns `BadRequest` error if client delievers 400 statusCode with body', () async {
+      mock_post_with(400, '{"any_key":"any_value"}');
+      final failure = sut.request(url: url, method: 'POST');
+      expect(failure, throwsA(DomainError.bad_request));
+    });
+
+    test('request returns `BadRequest` error if client delievers 400 statusCode withut body', () async {
+      mock_post_with(400, '');
+      final failure = sut.request(url: url, method: 'POST');
+      expect(failure, throwsA(DomainError.bad_request));
     });
   });
 }
